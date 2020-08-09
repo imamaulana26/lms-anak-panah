b182d4a28d72a97ca2295ea1ced12eea59a06f1d
imamaulana26 <imamaulana26.github@gmail.com>
2020-08-09 15:41:41 +0700

    fixed issue forum siswa

--- application/views/siswa/v_forum.php
+++ application/views/siswa/v_forum.php
@@ -106,23 +106,25 @@
 								<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
 									<div class="list-group nav flex-column nav-pills">
 										<?php $user = $this->session->userdata('username');
-										$new = '<span class="badge badge-danger float-right">New</span>';
-										$cek = $this->db->get_where('tbl_log_forum', ['nisn_siswa' => $user, 'id_forum' => $this->uri->segment(2)])->row_array();
-										$exp = explode('::', $cek['log_forum']);
+											$new = '<span class="badge badge-danger float-right">New</span>';
+											$cek = $this->db->get_where('tbl_log_forum', ['nisn_siswa' => $user, 'id_forum' => $this->uri->segment(2)])->row_array();
+											$exp = isset($cek['log_forum']) ? explode('::', $cek['log_forum']) : '';
 
-										foreach ($materi as $val) : ?>
+											foreach ($materi as $val) : ?>
 											<a href="#forum-<?= $val['pertemuan'] ?>" class="nav-link list-group-item <?= $val['pertemuan'] == $page ? 'active' : '' ?>" id="forum-<?= $val['pertemuan'] ?>-tab" aria-controls="forum-<?= $val['pertemuan'] ?>" data-toggle="pill" role="tab">
 												<h5>Forum Ke-<?= $val['pertemuan'] ?>
 													<?php $status = false;
-													foreach ($exp as $key => $n) {
-														if ($n == $val['pertemuan']) {
-															$status = true;
-														}
-													}
+															if ($exp) {
+																foreach ($exp as $key => $n) {
+																	if ($n == $val['pertemuan']) {
+																		$status = true;
+																	}
+																}
+															}
 
-													if ($status != true) {
-														echo $new;
-													} ?>
+															if ($status != true) {
+																echo $new;
+															} ?>
 												</h5>
 												<small><?= date('d M Y', strtotime($val['createDate'])) ?></small>
 												<p><?= word_limiter($val['judul_materi'], 2) ?></p>
@@ -142,141 +144,139 @@
 											</div>
 											<div class="card-body">
 												<p><?= $val['isi_materi'] ?></p>
-												<?php if ($val['jns_materi'] == 'Teori') : ?>
-													<hr>
-													<div>
-														<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $val['id_forum'] . '-' . $val['pertemuan'] ?>">
-															<i class="fa fa-fw fa-reply"></i> Balas
-														</a>
-														<span data-toggle="collapse" data-target="#collapseExample-<?= $val['id'] ?>" aria-expanded="false" aria-controls="collapseExample" style="cursor: pointer;">
-															<?php $li_komen = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan']])->num_rows(); ?>
-															Lihat Komentar (<?= $li_komen ?>)
-														</span>
-														<div class="collapse pt-3" id="show_komen-<?= $val['id_forum'] . '-' . $val['pertemuan'] ?>">
-															<div class="card card-body">
-																<form action="<?= site_url('forum/submit_main') ?>" method="post" autocomplete="off">
-																	<input type="hidden" name="id" id="id" value="<?= $val['id'] ?>">
-																	<input type="hidden" name="id_forum" id="id_forum" value="<?= $val['id_forum'] ?>">
-																	<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $val['pertemuan'] ?>">
-																	<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
-																	<div class="input-group">
-																		<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
-																		<div class="input-group-append">
-																			<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
-																		</div>
+												<hr>
+												<div>
+													<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $val['id_forum'] . '-' . $val['pertemuan'] ?>">
+														<i class="fa fa-fw fa-reply"></i> Balas
+													</a>
+													<span data-toggle="collapse" data-target="#collapseExample-<?= $val['id'] ?>" aria-expanded="false" aria-controls="collapseExample" style="cursor: pointer;">
+														<?php $li_komen = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan']])->num_rows(); ?>
+														Lihat Komentar (<?= $li_komen ?>)
+													</span>
+													<div class="collapse pt-3" id="show_komen-<?= $val['id_forum'] . '-' . $val['pertemuan'] ?>">
+														<div class="card card-body">
+															<form action="<?= site_url('forum/submit_main') ?>" method="post" autocomplete="off">
+																<input type="hidden" name="id" id="id" value="<?= $val['id'] ?>">
+																<input type="hidden" name="id_forum" id="id_forum" value="<?= $val['id_forum'] ?>">
+																<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $val['pertemuan'] ?>">
+																<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
+																<div class="input-group">
+																	<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
+																	<div class="input-group-append">
+																		<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
 																	</div>
-																</form>
-															</div>
+																</div>
+															</form>
 														</div>
 													</div>
+												</div>
 
-													<div class="collapse <?= $this->session->flashdata('page') ? 'show' : '' ?>" id="collapseExample-<?= $val['id'] ?>">
-														<!-- Main Comments -->
-														<div class="card-body">
-															<div class="row">
-																<?php $komen = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan'], 'reply_to' => 0]);
-																foreach ($komen->result_array() as $cmd) :
-																	$siswa = $this->db->get_where('tbl_siswa', ['siswa_nis' => $cmd['user_komen']])->row_array();
+												<div class="collapse <?= $this->session->flashdata('page') ? 'show' : '' ?>" id="collapseExample-<?= $val['id'] ?>">
+													<!-- Main Comments -->
+													<div class="card-body">
+														<div class="row">
+															<?php $komen = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan'], 'reply_to' => 0]);
+																	foreach ($komen->result_array() as $cmd) :
+																		$siswa = $this->db->get_where('tbl_siswa', ['siswa_nis' => $cmd['user_komen']])->row_array();
 
-																	$admin = $this->db->get_where('tbl_pengguna', ['pengguna_username' => $cmd['user_komen']])->row_array();
-																	$rep_user = ($siswa == null) ? $admin['pengguna_nama'] . ' (pengajar)' : $siswa['siswa_nama']; ?>
-																	<div class="card-header bordered mt-3 d-flex">
-																		<div class="col-md-1">
-																			<img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid" />
-																		</div>
-																		<div class="col-md">
-																			<strong class="float-left"><?= $rep_user ?></strong>
-																			<small class="float-right text-secondary"><?= date('d M Y H:i', strtotime($cmd['createDate'])) ?></small>
-																			<div class="clearfix"></div>
-																		</div>
+																		$admin = $this->db->get_where('tbl_pengguna', ['pengguna_username' => $cmd['user_komen']])->row_array();
+																		$rep_user = ($siswa == null) ? $admin['pengguna_nama'] . ' (pengajar)' : $siswa['siswa_nama']; ?>
+																<div class="card-header bordered mt-3 d-flex">
+																	<div class="col-md-1">
+																		<img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid" />
 																	</div>
-																	<div class="card-body bordered pb-0">
-																		<p><?= $cmd['isi_komen'] ?></p>
-																		<div>
-																			<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $cmd['id'] ?>">
-																				<i class="fa fa-fw fa-reply"></i> Balas
-																			</a>
-																			<span data-toggle="collapse" data-target="#comments-<?= $cmd['id'] ?>" aria-expanded="false" aria-controls="comments" style="cursor: pointer;">
-																				<?php $li_reply = $this->db->get_where('tbl_komentar', ['reply_to' => $cmd['id']])->num_rows(); ?>
-																				<i class="fa fa-fw fa-comments"></i> Komentar (<?= $li_reply ?>)
-																			</span>
-																		</div>
-																		<div class="collapse pt-3" id="show_komen-<?= $cmd['id'] ?>">
-																			<div class="card card-body">
-																				<form action="<?= site_url('forum/submit_komen') ?>" method="post" autocomplete="off">
-																					<input type="hidden" name="id" id="id" value="<?= $val['id'] ?>">
-																					<input type="hidden" name="id_forum" id="id_forum" value="<?= $cmd['id_forum'] ?>">
-																					<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $cmd['pertemuan'] ?>">
-																					<input type="hidden" name="mention" id="mention" value="<?= $cmd['user_komen'] ?>">
-																					<input type="hidden" name="reply_to" id="reply_to" value="<?= $cmd['id'] ?>">
-																					<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
-																					<div class="input-group">
-																						<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
-																						<div class="input-group-append">
-																							<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
-																						</div>
-																					</div>
-																				</form>
-																			</div>
-																		</div>
+																	<div class="col-md">
+																		<strong class="float-left"><?= $rep_user ?></strong>
+																		<small class="float-right text-secondary"><?= date('d M Y H:i', strtotime($cmd['createDate'])) ?></small>
+																		<div class="clearfix"></div>
 																	</div>
-
-																	<?php $reply = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan'], 'reply_to' => $cmd['id']]);
-																	foreach ($reply->result_array() as $rep) :
-																		$rep_siswa = $this->db->get_where('tbl_siswa', ['siswa_nis' => $rep['user_komen']])->row_array();
-																		$admin = $this->db->get_where('tbl_pengguna', ['pengguna_username' => $rep['user_komen']])->row_array();
-
-																		$rep_user = ($rep_siswa == null) ? $admin['pengguna_nama'] . ' (pengajar)' : $rep_siswa['siswa_nama'];
-																		$mention = $this->db->get_where('tbl_siswa', ['siswa_nis' => $rep['mention']])->row_array(); ?>
-																		<!-- Reply Main Comments -->
-																		<div class="collapse <?= $this->session->flashdata('mention') == $cmd['id'] ? 'show' : '' ?>" id="comments-<?= $cmd['id'] ?>">
-																			<div class="col-lg ml-3">
-																				<div class="card-header bordered mt-3 d-flex">
-																					<div class="col-md-1">
-																						<img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid" />
-																					</div>
-																					<div class="col-md">
-																						<strong class="float-left"><?= $rep_user ?></strong>
-																						<small class="float-right text-secondary"><?= date('d M Y H:i', strtotime($rep['createDate'])) ?></small>
-																						<div class="clearfix"></div>
+																</div>
+																<div class="card-body bordered pb-0">
+																	<p><?= $cmd['isi_komen'] ?></p>
+																	<div>
+																		<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $cmd['id'] ?>">
+																			<i class="fa fa-fw fa-reply"></i> Balas
+																		</a>
+																		<span data-toggle="collapse" data-target="#comments-<?= $cmd['id'] ?>" aria-expanded="false" aria-controls="comments" style="cursor: pointer;">
+																			<?php $li_reply = $this->db->get_where('tbl_komentar', ['reply_to' => $cmd['id']])->num_rows(); ?>
+																			<i class="fa fa-fw fa-comments"></i> Komentar (<?= $li_reply ?>)
+																		</span>
+																	</div>
+																	<div class="collapse pt-3" id="show_komen-<?= $cmd['id'] ?>">
+																		<div class="card card-body">
+																			<form action="<?= site_url('forum/submit_komen') ?>" method="post" autocomplete="off">
+																				<input type="hidden" name="id" id="id" value="<?= $val['id'] ?>">
+																				<input type="hidden" name="id_forum" id="id_forum" value="<?= $cmd['id_forum'] ?>">
+																				<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $cmd['pertemuan'] ?>">
+																				<input type="hidden" name="mention" id="mention" value="<?= $cmd['user_komen'] ?>">
+																				<input type="hidden" name="reply_to" id="reply_to" value="<?= $cmd['id'] ?>">
+																				<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
+																				<div class="input-group">
+																					<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
+																					<div class="input-group-append">
+																						<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
 																					</div>
 																				</div>
-																				<div class="card-body bordered pb-0">
-																					<p><b><?= $mention['siswa_nama'] ?></b> <?= $rep['isi_komen'] ?></p>
-																					<div>
-																						<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $rep['id'] ?>">
-																							<i class="fa fa-fw fa-reply"></i> Balas
-																						</a>
-																					</div>
-																					<div class="collapse pt-5" id="show_komen-<?= $rep['id'] ?>">
-																						<div class="card card-body">
-																							<form action="<?= site_url('forum/submit_komen') ?>" method="post" autocomplete="off">
-																								<input type="hidden" name="id" id="id" value="<?= $cmd['id'] ?>">
-																								<input type="hidden" name="id_forum" id="id_forum" value="<?= $rep['id_forum'] ?>">
-																								<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $rep['pertemuan'] ?>">
-																								<input type="hidden" name="mention" id="mention" value="<?= $rep['user_komen'] ?>">
-																								<input type="hidden" name="reply_to" id="reply_to" value="<?= $cmd['id'] ?>">
-																								<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
-																								<div class="input-group">
-																									<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
-																									<div class="input-group-append">
-																										<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
-																									</div>
+																			</form>
+																		</div>
+																	</div>
+																</div>
+
+																<?php $reply = $this->db->get_where('tbl_komentar', ['id_forum' => $val['id_forum'], 'pertemuan' => $val['pertemuan'], 'reply_to' => $cmd['id']]);
+																			foreach ($reply->result_array() as $rep) :
+																				$rep_siswa = $this->db->get_where('tbl_siswa', ['siswa_nis' => $rep['user_komen']])->row_array();
+																				$admin = $this->db->get_where('tbl_pengguna', ['pengguna_username' => $rep['user_komen']])->row_array();
+
+																				$rep_user = ($rep_siswa == null) ? $admin['pengguna_nama'] . ' (pengajar)' : $rep_siswa['siswa_nama'];
+																				$mention = $this->db->get_where('tbl_siswa', ['siswa_nis' => $rep['mention']])->row_array(); ?>
+																	<!-- Reply Main Comments -->
+																	<div class="collapse <?= $this->session->flashdata('mention') == $cmd['id'] ? 'show' : '' ?>" id="comments-<?= $cmd['id'] ?>">
+																		<div class="col-lg ml-3">
+																			<div class="card-header bordered mt-3 d-flex">
+																				<div class="col-md-1">
+																					<img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid" />
+																				</div>
+																				<div class="col-md">
+																					<strong class="float-left"><?= $rep_user ?></strong>
+																					<small class="float-right text-secondary"><?= date('d M Y H:i', strtotime($rep['createDate'])) ?></small>
+																					<div class="clearfix"></div>
+																				</div>
+																			</div>
+																			<div class="card-body bordered pb-0">
+																				<p><b><?= $mention['siswa_nama'] ?></b> <?= $rep['isi_komen'] ?></p>
+																				<div>
+																					<a class="float-right btn btn-sm" data-toggle="collapse" href="#show_komen-<?= $rep['id'] ?>">
+																						<i class="fa fa-fw fa-reply"></i> Balas
+																					</a>
+																				</div>
+																				<div class="collapse pt-5" id="show_komen-<?= $rep['id'] ?>">
+																					<div class="card card-body">
+																						<form action="<?= site_url('forum/submit_komen') ?>" method="post" autocomplete="off">
+																							<input type="hidden" name="id" id="id" value="<?= $cmd['id'] ?>">
+																							<input type="hidden" name="id_forum" id="id_forum" value="<?= $rep['id_forum'] ?>">
+																							<input type="hidden" name="pertemuan" id="pertemuan" value="<?= $rep['pertemuan'] ?>">
+																							<input type="hidden" name="mention" id="mention" value="<?= $rep['user_komen'] ?>">
+																							<input type="hidden" name="reply_to" id="reply_to" value="<?= $cmd['id'] ?>">
+																							<input type="hidden" name="user_komen" id="user_komen" value="<?= $this->session->userdata('user'); ?>">
+																							<div class="input-group">
+																								<input type="text" name="komentar" class="form-control" id="komentar" placeholder="Tulis balasan...">
+																								<div class="input-group-append">
+																									<button class="btn btn-info" type="submit"><i class="fa fa-fw fa-paper-plane"></i></button>
 																								</div>
-																							</form>
-																						</div>
+																							</div>
+																						</form>
 																					</div>
 																				</div>
 																			</div>
 																		</div>
-																		<!-- End of Reply Main Comments -->
-																	<?php endforeach; ?>
+																	</div>
+																	<!-- End of Reply Main Comments -->
 																<?php endforeach; ?>
-															</div>
-															<!-- End of Main Comments -->
+															<?php endforeach; ?>
 														</div>
+														<!-- End of Main Comments -->
 													</div>
-												<?php endif; ?>
+												</div>
 											</div>
 										</div>
 									<?php endforeach; ?>

83332e364e075e5866a6f8293909b0a848c58229
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-09-27 00:37:32 +0700

    fix array multidimensi online class

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -1,11 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Siswa extends CI_Controller{
-	function __construct(){
+class Siswa extends CI_Controller
+{
+	function __construct()
+	{
 		parent::__construct();
-		if($this->session->userdata('masuk') !=TRUE){
-			$url=base_url('login');
+		if ($this->session->userdata('masuk') != TRUE) {
+			$url = base_url('login');
 			redirect($url);
 		};
 		$this->load->model('m_siswa');
@@ -18,17 +20,45 @@ class Siswa extends CI_Controller{
 	}
 
 
-	function index(){
+	function index()
+	{
 		$this->load->view('admin/v_siswa');
 	}
 
-	function online_class() {
-		$data['li_peserta'] = $this->db->get_where('tbl_siswa', ['soft_deleted' => 0])->result_array();
+	function online_class()
+	{
+		$row = array();
+		$peserta = array();
+		$sql = $this->db->get_where('tbl_kelas', ['kelas_id <' => 16])->result_array();
+		
+		foreach ($sql as $sql) {
+			$qry = $this->db->get_where('tbl_siswa', ['siswa_kelas_id' => $sql['kelas_id']])->result_array();
+			
+			$peserta['kelas'] = $sql['kelas_nama'];
+			$siswa = array();
+			foreach ($qry as $val) {
+				array_push(
+					$siswa,
+					array(
+						'nis' => $val['siswa_nis'],
+						'nama' => $val['siswa_nama'],
+						'onclass' => $val['oc']
+					)
+				);
+			}
+			$peserta['siswa'] = $siswa;
+			$row[] = $peserta;
+		}
+		// var_dump($row);
+		// die;
+				
+		$data['li_peserta'] = $row;
 
 		$this->load->view('admin/v_online_class', $data);
 	}
 
-	function save_online_class() {
+	function save_online_class()
+	{
 		$this->db->update('tbl_siswa', ['oc' => 0], ['oc' => 1]);
 
 		$peserta = $this->input->post('peserta[]');
@@ -39,59 +69,7 @@ class Siswa extends CI_Controller{
 
 		redirect(site_url('siswa/online_class'));
 	}
-
-	function online_class_copy(){
-		$siswa = array();
-		$li_siswa = $this->db->get_where('tbl_siswa', ['soft_deleted' => 0])->result_array();
-		
-		foreach ($li_siswa as $key => $sis) {
-			$row = array();
-
-			$row[] = $sis['siswa_nis'];
-			$row[] = $sis['siswa_nama'];
-
-			$siswa[] = $row;
-		}
-
-		$output = array(
-			'li_peserta' => $siswa
-		);
-
-		// $data = array(
-		// 	array(
-		// 		'kelas' => 'Kelas I',
-		// 		'siswa' => array(
-		// 			array(
-		// 				'nis' => 123123,
-		// 				'nama' => 'Merik'
-		// 			),
-		// 			array(
-		// 				'nis' => 3124124124,
-		// 				'nama' => 'Imam'
-		// 			)
-		// 		)
-		// 	),
-		// 	array(
-		// 		'kelas' => 'Kelas II',
-		// 		'siswa' => array(
-		// 			array(
-		// 				'nis' => 512431,
-		// 				'nama' => 'Nugroho'
-		// 			),
-		// 			array(
-		// 				'nis' => 541234,
-		// 				'nama' => 'Maulana'
-		// 			),
-		// 			array(
-		// 				'nis' => 52311,
-		// 				'nama' => 'Ibrahim'
-		// 			)
-		// 		)
-		// 	)
-		// );
-
-		$this->load->view('admin/v_online_class', $output);
-	}
+	
 
 	public function list_siswa()
 	{
@@ -112,10 +90,10 @@ class Siswa extends CI_Controller{
 			// $aksi .= '<br><a href="keuangan"><i class="fa fa-fw fa-dollar"></i></a> ';
 			// $aksi .= ' <a href="javascript:void(0)" title="Detail Siswa" onclick="detail_siswa(' . "'" . $li['siswa_nis'] . "'" . ')"><i class="fa fa-fw fa-user"></i></a></center>';
 
-			$aksi = '<a href="'.site_url('siswa/edit_siswa/'.$li['siswa_nis']).'" class="label label-success col-md-12"><i class="fa fa-fw fa-edit"></i> Edit Siswa</a><br>';
-			$aksi .= '<a href="javascript:delete_siswa('."'".$li['siswa_nis']."'".','."'".$li['siswa_nama']."'".')" class="label label-danger col-md-12"><i class="fa fa-fw fa-trash"></i>Keluarkan Siswa</a><br>';
-			$aksi .= '<a href="javascript:void(0)" class="label label-info col-md-12" onclick="detail_siswa('."'".$li['siswa_nis']."'".')"><i class="fa fa-fw fa-info"></i> Detail Siswa</a><br>';
-			$aksi .= '<a href="javascript:void(0)" class="label label-warning col-md-12" onclick="send_msg('."'".$li['siswa_nis']."'".')"><i class="fa fa-fw fa-envelope-o"></i> Send Massage</a>';
+			$aksi = '<a href="' . site_url('siswa/edit_siswa/' . $li['siswa_nis']) . '" class="label label-success col-md-12"><i class="fa fa-fw fa-edit"></i> Edit Siswa</a><br>';
+			$aksi .= '<a href="javascript:delete_siswa(' . "'" . $li['siswa_nis'] . "'" . ',' . "'" . $li['siswa_nama'] . "'" . ')" class="label label-danger col-md-12"><i class="fa fa-fw fa-trash"></i>Keluarkan Siswa</a><br>';
+			$aksi .= '<a href="javascript:void(0)" class="label label-info col-md-12" onclick="detail_siswa(' . "'" . $li['siswa_nis'] . "'" . ')"><i class="fa fa-fw fa-info"></i> Detail Siswa</a><br>';
+			$aksi .= '<a href="javascript:void(0)" class="label label-warning col-md-12" onclick="send_msg(' . "'" . $li['siswa_nis'] . "'" . ')"><i class="fa fa-fw fa-envelope-o"></i> Send Massage</a>';
 			$row[] = $aksi;
 
 			$data[] = $row;
@@ -131,7 +109,8 @@ class Siswa extends CI_Controller{
 		exit;
 	}
 
-	function save_msg(){
+	function save_msg()
+	{
 		$data = array(
 			'inbox_kontak' => $this->input->post('nis'),
 			'inbox_pesan' => $this->input->post('pesan')
@@ -141,47 +120,54 @@ class Siswa extends CI_Controller{
 		exit();
 	}
 
-	function get_siswa($nis){
+	function get_siswa($nis)
+	{
 		$data = $this->db->get_where('tbl_siswa', ['siswa_nis' => $nis])->row_array();
-		echo json_encode($data); exit;
+		echo json_encode($data);
+		exit;
 	}
 
-	function add_siswa(){
-		$data['kelas']=$this->m_kelas->get_all_kelas();
-		$data['agama']=$this->m_agama->get_all_agama();
+	function add_siswa()
+	{
+		$data['kelas'] = $this->m_kelas->get_all_kelas();
+		$data['agama'] = $this->m_agama->get_all_agama();
 
 		$page = 'admin/v_add_siswa';
 
 		$this->load->view($page, $data);
 	}
 
-	function delete_siswa($id){
+	function delete_siswa($id)
+	{
 		// var_dump($id); exit();
 		$this->db->update('tbl_siswa', ['soft_deleted' => '1'], ['siswa_nis' => $id]);
 
-		redirect('siswa','refresh');
+		redirect('siswa', 'refresh');
 	}
 
-	function detail_siswa($id){
+	function detail_siswa($id)
+	{
 		$this->db->select('*');
 		$this->db->from('tbl_siswa a');
-		$this->db->join('tbl_orangtua b', 'a.siswa_nis=b.siswa_nis','inner');
-		$this->db->join('tbl_agama c', 'a.siswa_agama_id=c.agama_id','left');
-		$this->db->join('tbl_kelas d', 'a.siswa_kelas_id=d.kelas_id','left');
-		$this->db->join('tbl_satelit e', 'a.satelit=e.satelit_id','left');
+		$this->db->join('tbl_orangtua b', 'a.siswa_nis=b.siswa_nis', 'inner');
+		$this->db->join('tbl_agama c', 'a.siswa_agama_id=c.agama_id', 'left');
+		$this->db->join('tbl_kelas d', 'a.siswa_kelas_id=d.kelas_id', 'left');
+		$this->db->join('tbl_satelit e', 'a.satelit=e.satelit_id', 'left');
 		$this->db->where(['a.siswa_nis' => $id, 'a.soft_deleted' => '0']);
 
 		$result = $this->db->get()->row_array();
-		echo json_encode($result); exit;
+		echo json_encode($result);
+		exit;
 	}
 
-	function edit_siswa($nis){
+	function edit_siswa($nis)
+	{
 		$sql = "select * from tbl_siswa a ";
 		$sql .= "left join tbl_orangtua b on a.siswa_nis = b.siswa_nis ";
 		$sql .= "inner join tbl_kelas c on a.siswa_kelas_id = c.kelas_id ";
 		$sql .= "inner join tbl_agama d on a.siswa_agama_id = d.agama_id ";
 		$sql .= "inner join tbl_satelit e on a.satelit = e.satelit_id ";
-		$sql .= "where a.siswa_nis = '".$nis."' and a.soft_deleted = '0'";
+		$sql .= "where a.siswa_nis = '" . $nis . "' and a.soft_deleted = '0'";
 
 		$dt = $this->db->query($sql)->row_array();
 
@@ -239,29 +225,30 @@ class Siswa extends CI_Controller{
 		$this->load->view('admin/v_edit_siswa', $data);
 	}
 
-	function simpan_siswa(){
+	function simpan_siswa()
+	{
 		$nis = strip_tags($this->input->post('xnis'));
 
-		if((file_exists('./assets/filesiswa/'.$nis)) && (is_dir('./assets/filesiswa/'.$nis))){
+		if ((file_exists('./assets/filesiswa/' . $nis)) && (is_dir('./assets/filesiswa/' . $nis))) {
 			echo "<script>alert('NIS Sudah Ada!'); window.history.back();</script>";
 		} else {
 			// var_dump($_FILES); exit();
-			for($i = 0; $i <= 1; $i++){
-				if($_FILES['file'.$i]['error'] > 0){
-					$this->session->set_flashdata('msg','info');
+			for ($i = 0; $i <= 1; $i++) {
+				if ($_FILES['file' . $i]['error'] > 0) {
+					$this->session->set_flashdata('msg', 'info');
 					// header("location:javascript://history.go(-1)");
 					echo "<script>alert('Size not allowed!'); window.history.back();</script>";
 					return;
 				}
 			}
 
-			if($_FILES['file0']['type'] == 'application/pdf'){
-				$this->session->set_flashdata('msg','info');
-					// header("location:javascript://history.go(-1)");
+			if ($_FILES['file0']['type'] == 'application/pdf') {
+				$this->session->set_flashdata('msg', 'info');
+				// header("location:javascript://history.go(-1)");
 				echo "<script>alert('File foto harus berformat JPG/PNG'); window.history.back();</script>";
 				return;
 			}
-			mkdir('./assets/filesiswa/'.$nis);
+			mkdir('./assets/filesiswa/' . $nis);
 
 			$dt_siswa = array(
 				'siswa_nis' => $nis,
@@ -320,28 +307,28 @@ class Siswa extends CI_Controller{
 
 
 			$config = array(
-				'upload_path' => './assets/filesiswa/'.$nis,
+				'upload_path' => './assets/filesiswa/' . $nis,
 				'allowed_types' => 'jpg|jpeg|png|pdf'
 				// 'max_size' => '2000'
 			);
 
 			$this->upload->initialize($config);
 
-			for($i = 0; $i <= 1; $i++){
-				if($_FILES['file'.$i]['name']){
+			for ($i = 0; $i <= 1; $i++) {
+				if ($_FILES['file' . $i]['name']) {
 
-					$this->upload->do_upload('file'.$i);
+					$this->upload->do_upload('file' . $i);
 					$dt_upload = $this->upload->data();
 
-					if($dt_upload['is_image'] === true){
+					if ($dt_upload['is_image'] === true) {
 						$config['image_library'] = 'gd2';
-						$config['source_image'] = './assets/filesiswa/'.$nis.'/'.$dt_upload['file_name'];
-						$config['create_thumb']= FALSE;
-						$config['maintain_ratio']= FALSE;
-						$config['quality']= '60%';
-						$config['width']= 230;
-						$config['height']= 250;
-						$config['new_image'] = './assets/filesiswa/'.$nis.'/'.$dt_upload['file_name'];
+						$config['source_image'] = './assets/filesiswa/' . $nis . '/' . $dt_upload['file_name'];
+						$config['create_thumb'] = FALSE;
+						$config['maintain_ratio'] = FALSE;
+						$config['quality'] = '60%';
+						$config['width'] = 230;
+						$config['height'] = 250;
+						$config['new_image'] = './assets/filesiswa/' . $nis . '/' . $dt_upload['file_name'];
 						$this->load->library('image_lib', $config);
 						$this->image_lib->resize();
 
@@ -359,11 +346,12 @@ class Siswa extends CI_Controller{
 			$this->db->insert('tbl_orangtua', $dt_ortu);
 			$this->db->insert('tbl_pengguna', $dt_pengguna);
 
-			redirect('siswa','refresh');
+			redirect('siswa', 'refresh');
 		}
 	}
 
-	function update_siswa(){
+	function update_siswa()
+	{
 		// var_dump($_FILES);
 		// exit();
 		$nis = strip_tags($this->input->post('xnis'));
@@ -418,23 +406,23 @@ class Siswa extends CI_Controller{
 		);
 
 		$config = array(
-			'upload_path' => './assets/filesiswa/'.$nis,
+			'upload_path' => './assets/filesiswa/' . $nis,
 			'allowed_types' => 'jpg|jpeg|png|pdf'
 			// 'max_size' => '2000'
 		);
 
 		$this->upload->initialize($config);
-		$getsql = $this->db->get_where('tbl_siswa',['siswa_nis'=>$nis])->row_array();
+		$getsql = $this->db->get_where('tbl_siswa', ['siswa_nis' => $nis])->row_array();
 		$dt_ptlama = $getsql["siswa_photo"];
 		$dt_flama = $getsql["siswa_dokumen"];
 
 		// var_dump($dt_ptlama);
 		// exit();
 
-		for($i = 0; $i <= 1; $i++){
-			if($_FILES['file'.$i]['name']){
-				if($_FILES['file'.$i]['error'] > 0){
-					$this->session->set_flashdata('msg','info');
+		for ($i = 0; $i <= 1; $i++) {
+			if ($_FILES['file' . $i]['name']) {
+				if ($_FILES['file' . $i]['error'] > 0) {
+					$this->session->set_flashdata('msg', 'info');
 					// header("location:javascript://history.go(-1)");
 					echo "<script>alert('Size not allowed!'); window.history.back();</script>";
 					return;
@@ -442,26 +430,26 @@ class Siswa extends CI_Controller{
 					// $path='./assets/filesiswa/'.$nis.'/'.$dt_ptlama;
 					// exit();
 
-					$this->upload->do_upload('file'.$i);
+					$this->upload->do_upload('file' . $i);
 					$dt_upload = $this->upload->data();
 
-					if($dt_upload['is_image'] === true){
+					if ($dt_upload['is_image'] === true) {
 						$config['image_library'] = 'gd2';
-						$config['source_image'] = './assets/filesiswa/'.$nis.'/'.$dt_upload['file_name'];
-						$config['create_thumb']= FALSE;
-						$config['maintain_ratio']= FALSE;
-						$config['quality']= '60%';
-						$config['width']= 230;
-						$config['height']= 250;
-						$config['new_image'] = './assets/filesiswa/'.$nis.'/'.$dt_upload['file_name'];
+						$config['source_image'] = './assets/filesiswa/' . $nis . '/' . $dt_upload['file_name'];
+						$config['create_thumb'] = FALSE;
+						$config['maintain_ratio'] = FALSE;
+						$config['quality'] = '60%';
+						$config['width'] = 230;
+						$config['height'] = 250;
+						$config['new_image'] = './assets/filesiswa/' . $nis . '/' . $dt_upload['file_name'];
 						$this->load->library('image_lib', $config);
-						$this->image_lib->resize();	
+						$this->image_lib->resize();
 
-						unlink('./assets/filesiswa/'.$nis.'/'.$dt_ptlama);
+						unlink('./assets/filesiswa/' . $nis . '/' . $dt_ptlama);
 						// exit();
 						$dt_siswa['siswa_photo'] = $dt_upload['file_name'];
 					} else {
-						unlink('./assets/filesiswa/'.$nis.'/'.$dt_flama);
+						unlink('./assets/filesiswa/' . $nis . '/' . $dt_flama);
 						$dt_siswa['siswa_dokumen'] = $dt_upload['file_name'];
 					}
 				}
@@ -472,6 +460,6 @@ class Siswa extends CI_Controller{
 		$this->db->update('tbl_orangtua', $dt_ortu, ['siswa_nis' => $nis]);
 		// exit();
 
-		redirect('siswa','refresh');
+		redirect('siswa', 'refresh');
 	}
 }

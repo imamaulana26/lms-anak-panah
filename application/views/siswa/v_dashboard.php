77b603c833fe219573907318b219f8ce3ff0e892
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-12-05 18:13:53 +0700

    perbaikan layout tampilan mobile

--- application/views/siswa/v_dashboard.php
+++ application/views/siswa/v_dashboard.php
@@ -27,7 +27,7 @@
 
 			<!-- Tagihan -->
 			<div class="row">
-				<div class="offset-1 col-sm-10">
+				<div class="offset-md-1 col-md-10 col-sm">
 					<!-- Index Prestasi -->
 					<div class="card card-primary card-outline">
 						<div class="card-header">
@@ -37,20 +37,20 @@
 						<div class="card-body">
 							<div class="row">
 								<div class="col-sm">
-									<table class="table">
+									<table class="table table-responsive" style="white-space: nowrap;">
 										<thead>
 											<tr>
-												<th scope="col">#</th>
-												<th scope="col">Jenis Tagihan</th>
-												<th scope="col">Tgl. Jatuh Tempo</th>
-												<th scope="col">Nominal</th>
-												<th scope="col">Sisa Tagihan</th>
+												<th>#</th>
+												<th>Jenis Tagihan</th>
+												<th>Jatuh Tempo</th>
+												<th>Nominal</th>
+												<th>Sisa Tagihan</th>
 											</tr>
 										</thead>
 										<tbody>
 											<?php $no = 1;
 											$tagihan = $this->db->select('*')->from('tbl_pembayaran a')->join('tbl_tagihan b', 'a.jns_tagihan = b.id_tagihan', 'inner')
-											->where('a.nis_siswa', $this->session->userdata('username'))->get()->result_array();
+												->where('a.nis_siswa', $this->session->userdata('username'))->get()->result_array();
 											foreach ($tagihan as $tgh) { ?>
 												<tr>
 													<td><?= $no++ ?></td>
@@ -70,7 +70,7 @@
 			</div><!-- /.row -->
 
 			<div class="row">
-				<div class="offset-1 col-sm-10">
+				<div class="offset-md-1 col-md-10 col-sm">
 					<!-- Index Prestasi -->
 					<div class="card card-primary card-outline">
 						<div class="card-header">
@@ -79,7 +79,7 @@
 						</div>
 						<div class="card-body">
 							<div class="row">
-								<div class="offset-1 col-sm-10">
+								<div class="offset-md-1 col-md-10 col-sm">
 									<canvas id="myChart" style="height: 15px;"></canvas>
 								</div>
 							</div>
@@ -89,54 +89,94 @@
 			</div><!-- /.row -->
 
 			<div class="row">
-				<div class="offset-1 col-sm-10">
+				<div class="offset-md-1 col-md-10 col-sm">
 					<!-- Course -->
 					<div class="card card-primary card-outline">
 						<div class="card-header">
-							<h5 class="card-title m-0">Recent Course</h5>
-							<a href="<?= site_url('course') ?>" class="m-auto" style="float: right; position: relative;">View All</a>
+							<h5 class="card-title m-0">Penilaian</h5>
+							<!-- <a href="<?= site_url('course') ?>" class="m-auto" style="float: right; position: relative;">View All</a> -->
 						</div>
 						<div class="card-body">
-							<div class="row">
-								<?php $user = $this->session->userdata('username');
-
-								$log = $this->db->get_where('tbl_log_forum', ['nisn_siswa' => $user])->result_array();
-								$dt_user = $this->db->get_where('tbl_siswa', ['siswa_nis' => $user])->row_array();
-
-								foreach ($log as $log) {
-									$exp_forum = explode('::', $log['log_forum']);
-									$n_forum = $exp_forum[0] == '' ? 0 : count($exp_forum);
-
-									$exp_tugas = explode('::', $log['log_tugas']);
-									$n_tugas = $exp_tugas[0] == '' ? 0 : count($exp_tugas);
-
-									$li_materi = $this->db->select('a.id_pelajaran, b.nm_mapel')
-									->from('tbl_pelajaran a')
-									->join('tbl_mapel b', 'a.kd_mapel = b.kd_mapel', 'left')
-									->where(['a.id_kelas' => $dt_user['siswa_kelas_id'], 'a.id_pelajaran' => $log['id_forum']])
-									->group_by('a.id_pelajaran')->get()->result_array();
-
-									foreach ($li_materi as $li) : ?>
-										<div class="col-sm-4">
-											<div class="card">
-												<div class="card-img-caption">
-													<img class="card-img-top" src="<?= base_url('assets/front-end/dist/img/gradient.jpg') ?>">
-													<strong class="card-text" id="title"><?= $li['nm_mapel']; ?></strong>
-													<p>OS1 - 1721 - ISYS6304 - THBA</p>
-												</div>
-												<div class="card-body">
-													<?php $jml_forum = $this->db->get_where('tbl_materi_forum', ['id_forum' => $li['id_pelajaran']])->num_rows(); ?>
-													<a href="<?= site_url('forum/') . $li['id_pelajaran'] ?>" id="forum"><i class="fas fa-fw fa-comments pr-1"></i> <?= $jml_forum - $n_forum ?> forum posting</a>
-													<div class="dropdown-divider"></div>
-													<?php $jml_tugas = $this->db->get_where('tbl_materi_tugas', ['id_forum' => $li['id_pelajaran']])->num_rows(); ?>
-													<a href="<?= site_url('tugas/') . $li['id_pelajaran'] ?>"><i class="fas fa-fw fa-tasks pr-1"></i> <?= $jml_tugas - $n_tugas ?> Assigment to do</a>
-													<div class="dropdown-divider"></div>
-													<a href="javascript:void(0)" id="view" data-toggle="modal" data-target="#modal_schedule"><i class="fas fa-fw fa-clipboard-list pr-1"></i> View schedule</a>
-												</div>
-											</div>
+							<div class="row display">
+								<div class="col-md-4 col-sm">
+									<div class="card card-primary bg-light mb-3">
+										<div class="card-header">Adyakhansa Mustika Jagatwanata</div>
+										<div class="card-body">
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Petemuan</th>
+														<th>Nilai</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>Forum ke-1</td>
+														<td>75 point</td>
+													</tr>
+													<tr>
+														<td>Tugas ke-1</td>
+														<td>25 point</td>
+													</tr>
+												</tbody>
+											</table>
 										</div>
-									<?php endforeach;
-								} ?>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm">
+									<div class="card card-primary bg-light mb-3">
+										<div class="card-header">Afnan Al Kautzar</div>
+										<div class="card-body">
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Petemuan</th>
+														<th>Nilai</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>Forum ke-1</td>
+														<td>35 point</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm">
+									<div class="card card-primary bg-light mb-3">
+										<div class="card-header">Alfarezha Prasetya V</div>
+										<div class="card-body">
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Petemuan</th>
+														<th>Nilai</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>Forum ke-1</td>
+														<td>45 point</td>
+													</tr>
+													<tr>
+														<td>Forum ke-2</td>
+														<td>25 point</td>
+													</tr>
+													<tr>
+														<td>Tugas ke-1</td>
+														<td>50 point</td>
+													</tr>
+													<tr>
+														<td>Tugas ke-2</td>
+														<td>45 point</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -146,7 +186,7 @@
 	</div>
 	<!-- /.content -->
 
-	<?php $this->load->view('siswa/v_schedule'); ?>
+	<?php // $this->load->view('siswa/v_schedule'); ?>
 </div>
 <!-- /.content-wrapper -->
 
@@ -163,20 +203,20 @@
 				label: '# of Votes',
 				data: [12, 19, 3, 5, 2, 3],
 				backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
 				],
 				borderColor: [
-				'rgba(255, 99, 132, 1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
 				],
 				borderWidth: 1
 			}]
